0b9475b63f94f19c9203234636fc2ced219b27fd
MaverickTushar007 <tusharbhatt.official.2004@email.com>
2026-03-31 22:08:37 +0530

    fix: disable no-unescaped-entities eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = defineConfig([
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "react-hooks/set-state-in-effect": "off",
+      "react/no-unescaped-entities": "off",
       "@typescript-eslint/no-unused-vars": "warn",
     },
   },
